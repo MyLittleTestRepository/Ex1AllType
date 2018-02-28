df25e0673ab6f58e2ba001ddc7980d04cafdae07
Averin Dmitry <averin@intaro.email>
2018-02-28 16:31:03 +0300

    ex1-1

---
+++ partners/.left.menu.php
@@ -0,0 +1,25 @@
+<?
+$aMenuLinks = Array(
+	Array(
+		"Условия сотрудничества", 
+		"/partners/index.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Как стать партнером", 
+		"/partners/to_new_partners.php", 
+		Array(), 
+		Array(), 
+		"" 
+	),
+	Array(
+		"Расписание мероприятий", 
+		"/partners/events/", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
---
+++ partners/events/.left.menu.php
@@ -0,0 +1,18 @@
+<?
+$aMenuLinks = Array(
+    Array(
+        "Анонсы",
+        "/partners/events/index.php",
+        Array(),
+        Array(),
+        ""
+    ),
+	Array(
+		"Архив", 
+		"/partners/events/old_events.php", 
+		Array(), 
+		Array(), 
+		"" 
+	)
+);
+?>
