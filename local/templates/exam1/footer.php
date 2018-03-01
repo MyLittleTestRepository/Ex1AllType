1c16d0d62614042d98b68dfd281049689496e4be
Averin Dmitry <averin@intaro.email>
2018-03-01 10:29:08 +0300

    ex1-6 insert left menu

--- local/templates/exam1/footer.php
+++ local/templates/exam1/footer.php
@@ -6,6 +6,25 @@
 <!-- /content -->
 <!-- side -->
 <div class="side">
+    <!-- side menu -->
+    <?$APPLICATION->IncludeComponent(
+        "bitrix:menu",
+        "left",
+        Array(
+            "ALLOW_MULTI_SELECT" => "N",
+            "CHILD_MENU_TYPE" => "left",
+            "DELAY" => "N",
+            "MAX_LEVEL" => "1",
+            "MENU_CACHE_GET_VARS" => array(""),
+            "MENU_CACHE_TIME" => "3600000",
+            "MENU_CACHE_TYPE" => "A",
+            "MENU_CACHE_USE_GROUPS" => "Y",
+            "MENU_THEME" => "site",
+            "ROOT_MENU_TYPE" => "left",
+            "USE_EXT" => "Y"
+        )
+    );?>
+    <!-- /side menu -->
     <!-- side anonse -->
     <div class="side-block side-anonse">
         <div class="title-block"><span class="i i-title01"></span>Полезная информация!</div>
