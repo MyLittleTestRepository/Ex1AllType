d6b9e3231b397893d9ff2c6911ee0744929dd29a
Averin Dmitry <averin@intaro.email>
2018-03-02 09:15:27 +0300

    ex1-12 add new snippet

---
+++ local/templates/exam1/snippets/.content.php
@@ -0,0 +1,6 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+use \Bitrix\Main\Localization\Loc;
+Loc::loadMessages(__FILE__);
+$SNIPPETS = Array();
+$SNIPPETS['snippet0001.snp'] = Array('title' => Loc::getMessage('title'));
+?>
