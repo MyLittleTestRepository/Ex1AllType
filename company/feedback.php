5da8f1eb3a9f3a2570efa912061c23a98eaea2f9
Averin Dmitry <averin@intaro.email>
2018-03-02 14:36:57 +0300

    ex1-18 end. add feedback form

--- company/feedback.php
+++ company/feedback.php
@@ -2,7 +2,18 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Обратная связь");
 ?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.feedback",
-	"",
-Array()
+	"bitrix:main.feedback", 
+	".default", 
+	array(
+		"COMPONENT_TEMPLATE" => ".default",
+		"USE_CAPTCHA" => "Y",
+		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+		"EMAIL_TO" => "admin@ad.min",
+		"REQUIRED_FIELDS" => array(
+		),
+		"EVENT_MESSAGE_ID" => array(
+			0 => "29",
+		)
+	),
+	false
 );?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
