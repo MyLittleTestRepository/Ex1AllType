df632dae1b74aa5c76f1c1fffe95e5455bfc90b8
Averin Dmitry <averin@intaro.email>
2018-03-05 08:42:32 +0300

    ex1-20 set auth form

--- local/templates/exam1/components/bitrix/system.auth.form/demo/template.php
+++ local/templates/exam1/components/bitrix/system.auth.form/demo/template.php
@@ -43,7 +43,7 @@ if($arResult["FORM_TYPE"] == "login")
 					<input type="password" placeholder="<?=GetMessage("AUTH_PASSWORD")?>" name="USER_PASSWORD" maxlength="50" size="17" autocomplete="off" />			
 				</div>
 				<div class="frm-row">
-					<a href="" class="btn-forgot"><?=GetMessage("AUTH_FORGOT_PASSWORD_2")?></a>
+					<a href="<?=$arResult["AUTH_FORGOT_PASSWORD_URL"]?>" class="btn-forgot"><?=GetMessage("AUTH_FORGOT_PASSWORD_2")?></a>
 				</div>
 				<div class="frm-row">
 					<div class="frm-chk">
@@ -54,9 +54,20 @@ if($arResult["FORM_TYPE"] == "login")
 					<input type="submit" name="Login" value="<?=GetMessage("AUTH_LOGIN_BUTTON")?>">
 				</div>
 			</form></li>
-		<li><a href=""><?=GetMessage("AUTH_REGISTER")?></a></li>
+		<li><a href="<?=$arResult["AUTH_REGISTER_URL"]?>"><?=GetMessage("AUTH_REGISTER")?></a></li>
 	</ul>
 </nav>
 <?
 }
 ?>
+<?if($arResult["FORM_TYPE"] == "logout"):?>
+    <nav class="menu-block">
+        <ul>
+            <li>
+                <a href="<?=$arResult["PROFILE_URL"]?>" ><?=$arResult["USER_NAME"]?> [<?=$arResult["USER_LOGIN"]?>]</a>
+            </li>
+            <li><a href="/?logout=yes"><?=GetMessage("AUTH_LOGOUT_BUTTON")?></a>
+            </li>
+        </ul>
+    </nav>
+<?endif;?>
